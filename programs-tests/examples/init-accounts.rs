d8375a88da9f2f296a74c741f5f49628fea8043a
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-04-13 12:28:53 +0400

    replace js migration with rust example init-accounts

---
+++ programs-tests/examples/init-accounts.rs
@@ -0,0 +1,83 @@
+use anchor_client::anchor_lang::prelude::system_program;
+use anchor_client::{
+  anchor_lang::prelude::Pubkey,
+  solana_sdk::commitment_config::CommitmentConfig,
+  solana_sdk::signature::Signer, Client, Cluster,
+};
+use programs_tests::sync_confirm_transaction;
+use sol_usd_oracle::{self, state::OracleState};
+use solana_keypair::read_keypair_file;
+use token_minter::state::MinterConfig;
+
+const INITIAL_PRICE: u64 = 120_000_000;
+const MINT_FEE_USD: u64 = 5_000_000;
+
+fn main() -> anyhow::Result<()> {
+  let oracle_program_id = "24UJLhNSDEwFrziTkshg6Rt18K7H3RczKXR8fNpQ8xa3";
+  let minter_program_id = "DXm5uV6Zh3HZshCSUtfoodGDuyDrKnzmP3Nq29PTmYrU";
+  let payer = read_keypair_file("/home/q/.config/solana/id.json").unwrap();
+
+  let client = Client::new_with_options(
+    Cluster::Localnet,
+    &payer,
+    CommitmentConfig::confirmed(),
+  );
+  let oracle_program_id = Pubkey::try_from(oracle_program_id)?;
+  let minter_program_id = Pubkey::try_from(minter_program_id)?;
+  let oracle_program = client.program(oracle_program_id)?;
+  let minter_program = client.program(minter_program_id)?;
+
+  let (oracle_pda, _bump) = Pubkey::find_program_address(
+    &[OracleState::SEED, &payer.pubkey().as_ref()],
+    &oracle_program_id,
+  );
+  let (mint_config_pda, mint_config_bump) = Pubkey::find_program_address(
+    &[MinterConfig::SEED, &payer.pubkey().as_ref()],
+    &minter_program_id,
+  );
+
+  // Initialize oracle
+  let signature = oracle_program
+    .request()
+    .accounts(sol_usd_oracle::accounts::InitializeOracle {
+      admin: payer.pubkey(),
+      oracle: oracle_pda,
+      system_program: system_program::ID,
+    })
+    .args(sol_usd_oracle::instruction::InitializeOracle {
+      admin: payer.pubkey(),
+    })
+    .send()?;
+  sync_confirm_transaction(&oracle_program, &signature)?;
+
+  // Update oracle price
+  oracle_program
+    .request()
+    .accounts(sol_usd_oracle::accounts::UpdatePrice {
+      admin: payer.pubkey(),
+      oracle: oracle_pda,
+    })
+    .args(sol_usd_oracle::instruction::UpdatePrice {
+      new_price: INITIAL_PRICE,
+    })
+    .send()?;
+
+  // Initialize mint config account
+  let signature = minter_program
+    .request()
+    .accounts(token_minter::accounts::InitializeMinter {
+      admin: payer.pubkey(),
+      config: mint_config_pda,
+      system_program: system_program::ID,
+    })
+    .args(token_minter::instruction::InitializeMinter {
+      treasury: payer.pubkey(),
+      mint_fee_usd: MINT_FEE_USD,
+      oracle_state: oracle_pda,
+      oracle_program: sol_usd_oracle::ID,
+    })
+    .send()?;
+  sync_confirm_transaction(&minter_program, &signature)?;
+
+  Ok(())
+}
