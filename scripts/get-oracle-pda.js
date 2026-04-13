e1b40afea22ff5efddf2eced00d42ff1c4b018fb
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-04-13 11:03:15 +0400

    code refactor
    - move anchor project as root
    - move BE and FE to modules folder

---
+++ scripts/get-oracle-pda.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// Run from repo root: node program/scripts/get-oracle-pda.js
+// Or from program/: node scripts/get-oracle-pda.js
+const anchor = require("@coral-xyz/anchor");
+const {PublicKey} = anchor.web3;
+const ORACLE_PROGRAM_ID = new PublicKey("2qTPCfB4yVywxyVWbsR4LxmgZSschrBVMre5kfeLRSxv");
+const [oraclePda] = PublicKey.findProgramAddressSync(
+  [Buffer.from("oracle_state")],
+  ORACLE_PROGRAM_ID
+);
+console.log("ORACLE_STATE_PUBKEY=" + oraclePda.toBase58());
